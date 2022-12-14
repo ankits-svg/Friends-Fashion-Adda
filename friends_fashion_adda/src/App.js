f9906d442f869122f63d64de95cc8f6aac897390
Ankit Sharma <ankits745@gmail.com>
2022-12-14 21:41:13 +0530

    day2

---
+++ friends_fashion_adda/src/App.js
@@ -0,0 +1,14 @@
+import Products from './Components/Products';
+import './App.css';
+import Navbar from './Pages/Navbar'
+function App() {
+  return (
+    <div className="App">
+      <h1>Bewakoof.com</h1>
+      {/* <Navbar/> */}
+      <Products/>
+    </div>
+  );
+}
+
+export default App;
---
+++ friends_fashion_adda/src/Pages/Navbar.jsx
@@ -0,0 +1,108 @@
+import { ReactNode } from 'react';
+import {
+  Box,
+  Flex,
+  Avatar,
+  Link,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  Center,Image,Text, FormControl, Input
+} from '@chakra-ui/react';
+
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+import image from '../Images/Logo.png'
+
+const NavLink = ({ children }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded={'md'}
+    _hover={{
+      textDecoration: 'none',
+      bg: useColorModeValue('#F9BA00', 'gray'),
+    }}
+    href={'#'}>
+    {children}
+  </Link>
+);
+
+export default function Navbar() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Box border='1px' borderColor='gray.200' bg={useColorModeValue('white', 'gray')} px={4}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <Box>
+            <Image w='15%' src={image} alt="error"/>
+          </Box>
+          
+          <Text>MEN</Text>
+          <Text>WOMEN</Text>
+          <Text>MOBILE COVERS</Text>
+          <FormControl action="">
+            <Input 
+            type="search"
+            placeholder='Search here...'
+            />
+          </FormControl>
+          
+          
+
+          
+            
+            
+        
+
+          <Flex alignItems={'center'}>
+            <Stack direction={'row'} spacing={7}>
+              <Button onClick={toggleColorMode}>
+                {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+              </Button>
+
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  rounded={'full'}
+                  variant={'link'}
+                  cursor={'pointer'}
+                  minW={0}>
+                  <Avatar
+                    size={'sm'}
+                    src={'https://avatars.dicebear.com/api/male/username.svg'}
+                  />
+                </MenuButton>
+                <MenuList alignItems={'center'}>
+                  <br />
+                  <Center>
+                    <Avatar
+                      size={'2xl'}
+                      src={'https://avatars.dicebear.com/api/male/username.svg'}
+                    />
+                  </Center>
+                  <br />
+                  <Center>
+                    <p>Username</p>
+                  </Center>
+                  <br />
+                  <MenuDivider />
+                  <MenuItem>Your Servers</MenuItem>
+                  <MenuItem>Account Settings</MenuItem>
+                  <MenuItem>Logout</MenuItem>
+                </MenuList>
+              </Menu>
+            </Stack>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  );
+}
